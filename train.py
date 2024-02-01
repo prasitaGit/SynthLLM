f71f6a2f5459e1eb124a86a0703e8182ba846e6f
Prasita Mukherjee <prasitamukherjee@Prasitas-MBP-2.franklinparkapartments.com>
2024-02-01 11:10:04 -0500

    Cleanup

--- train.py
+++ train.py
@@ -1,19 +1,28 @@
 from openai import OpenAI
 from subprocess import call
 client = OpenAI()
-
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "Translate Gallina program to C program. Only include the C program in the content. No need to include a main function in the translated C program, and other English description"},
-    {"role": "user", "content": "Convert Definition swapnum (a b : Z) := (b, a) to C with a and b as pointers. Output the C program only, and not any English with it"}
+messages=[
+   {"role": "system", "content": "Translate separation logic specification to C program. Only include the C program in the content. No need to include a main function in the translated C program, and other English description"},
+    {"role": "user", "content": "{x |-> a * y |-> b}void swapnum(int* x, int* y){ x |-> b * y |-> a }"},
+    {"role": "assistant", "content": "void swapnum(int* x, int* y) {  int temp = *x; *x = *y; *y = temp;}"}
   ]
-)
-#print(completion)
-cprog = completion.choices[0].message.content
-#cprog = "void swapnum(int* a, int* b) {  int temp = *a; *a = *b; *b = temp;}"
-fcprog = open("swapprog.c","w")
-fcprog.write(cprog)
-fcprog.close()
-#call clightgen
-call(["clightgen", "-normalize","swapprog.c"])
+
+while True:
+    uinp = input("Please enter your specification in separation logic format: (Pre-condition)function name(Post-condition), otherwise Quit: ")
+    if uinp == 'Quit':
+      break
+    messages.append({"role":"user","content":uinp})
+    #one-shot experience - just give the prog. doesnt work
+    completion = client.chat.completions.create(
+      model="gpt-3.5-turbo",
+      seed=1235,
+      messages=messages
+    )
+
+    cprog = completion.choices[0].message.content
+    #print(cprog)
+    fcprog = open("proggen.c","w")
+    fcprog.write(cprog)
+    fcprog.close()
+    #call clightgen
+    #call(["clightgen", "-normalize","swapprog.c"])
